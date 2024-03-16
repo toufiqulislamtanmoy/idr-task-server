baddd4e1274df47be1a2c259eea5f7240a12a75d
toufiqulislamtanmoy <tittanmoy@gmail.com>
2024-03-16 16:31:05 +0600

    repo setup

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+
+const app = express();
+const port = 3001;
+
+const db = new sqlite3.Database('./dua_main.sqlite');
+
+app.get("/", (req, res) => {
+    res.send("Sqlite is running")
+})
+
+app.get('/api/category', (req, res) => {
+    db.all('SELECT * FROM category', (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        res.status(200).json(rows);
+    });
+});
+app.get('/api/sub-category/:cat_id', (req, res) => {
+    const cat_id = req.params.cat_id;
+    db.all(`SELECT * FROM sub_category WHERE cat_id = ?`, [cat_id], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        res.status(200).json(rows);
+    });
+});
+
+app.get('/api/dua/:cat_id', (req, res) => {
+    const cat_id = req.params.cat_id;
+    db.all(`SELECT * FROM dua WHERE cat_id = ?`, [cat_id], (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
+        }
+
+        res.status(200).json(rows);
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Backend server running at http://localhost:${port}`);
+});
